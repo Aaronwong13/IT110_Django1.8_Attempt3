f080713d3505da5188d319b4d0ed5005f2b13fc4
Cloud617 <Aronwong95@gmail.com>
2015-12-23 11:56:55 -0600

    I have updated my Views.py & Test.py
    The reason why I updated
    Views. py and Test.py
    so I can added more test in the
    Polls App.
    Reported-By: Aaron Wong
    Signed-Off-By: Aaron Wong <Aaron.Wong001@umb.edu>

--- polls/views.py
+++ polls/views.py
@@ -5,15 +5,21 @@ from .models import Question
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+     def get_queryset(self):
+          """
+         Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
